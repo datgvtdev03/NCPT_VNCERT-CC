9cac830e0519026e9c3c77eaab9afa165acb0816
yenyen <yenpt0812@gmail.com>
2022-10-28 23:33:35 +0700

    Pham Home VNCERT

---
+++ Home_VNCERT/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <div class="tong">
+    <header>
+    <div class="container2">
+            <div class="img">
+                <img src="./img/Screen_Shot_2022-10-16_at_22.31.52-removebg-preview.png" alt="">
+
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="https://vncert.vn/posts/6/hoat-dong-kiem-dinh">Trang chủ</a></li>
+                    <li><a href="#https://vncert.vn/posts/3/hoat-dong-mang-luoi-ucsc">Kiểm định</a></li>
+                    <li><a href="https://chongthurac.vn/">Chống thư rác</a></li>
+                    <li><a href="https://vn-cop.vn/">Bảo vệ trẻ em</a></li>
+                    <li><a href="https://vncert.vn/posts/30/tuyen-dung">Tuyển dụng</a></li>
+                 
+                </ul>
+            
+            </div>
+          
+  
+           
+           
+           
+            
+        </div>
+        
+        <div class="container">
+            <div class="box1">
+                <img src="img/dienthoai-removebg-preview.png" alt="">
+                <span>0976.198.642</span>
+                <img src="img/mail-removebg-preview.png" alt="">
+                <span>www.vncert.gov.vn</span>
+                
+                
+            </div>
+            <div class="box2">
+                <img src="img/ws-removebg-preview.png" alt="">
+                <a href="https://vncert.gov.vn/?fbclid=IwAR1tLqy2XYo8axrE1zdO2nz_7zZSqQZKNYJI46qOmW3gdvT59YoE_s5YtL8">Website</a>
+
+                <img src="img/fb-removebg-preview.png" alt="">
+                <a href="https://www.facebook.com/Vncertcc/">Facebook</a>
+
+                <!-- <button>Search</button> -->
+                
+                    <input type="text" class="searchTerm" placeholder="Bạn muốn tìm gì?">
+                    <button type="submit" class="searchButton">
+                        Submit
+                    </button>
+
+                
+            </div>
+        </div>
+       
+        <div class="img2">
+                <img src="./img/bien.jpeg" alt="" width="100%">
+
+            </div>
+        
+    </header>
+    <div class="body">
+        <div class="chu">
+            <h1>Trung tâm ứng cứu khẩn cấp mạng Việt Nam</h1>
+            <p style="font-size: 20px;line-height: 40px;">  -VNCERT/CC - Cơ quan điều phối quốc gia về ứng cứu sự cố, thuộc Cục An toàn thông tin <br>
+                - Bộ Thông tin và Truyền thông. Có chức năng điều phối, ứng cứu các sự cố an toàn thông tin mạng trên toàn quốc.<br>
+                Trung tâm Ứng cứu khẩn cấp không gian mạng Việt Nam là đơn vị sự nghiệp công lập trực thuộc Cục An toàn thông tin <br>
+                – Bộ Thông tin và Truyền thông (viết tắt là VNCERT/CC), thực hiện chức năng là đầu mối kỹ thuật điều phối ứng cứu sự cố an toàn không gian mạng và kiểm định an toàn thông tin trên phạm vi toàn quốc<br>
+                - Quản lý, vận hành các hệ thống số liệu, cơ sở dữ liệu, hệ thống kỹ thuật về điều phối ứng cứu sự cố, kiểm định và phòng, chống thư điện tử rác<br>
+                - Tin nhắn rác phục vụ công tác quản lý nhà nước và thực thi pháp luật về an toàn thông tin; là đầu mối hợp tác quốc tế với các cơ quan, tổ chức có chức năng ứng cứu sự cố và kiểm định an toàn thông tin.</p>
+
+        </div>
+        
+        <img src="https://vncert.vn/banner.webp" alt="" width="46%">
+
+        
+
+    </div>
+    <hr>
+    <center>
+        <div>
+            <h2 style="color: red;"  >THÁNG HƯỞNG ỨNG NGÀY CHUYỂN DỔI SỐ QUỐC TẾ</h2>
+            <hr style="size:18px ;">Ngày 10/10/2022
+    </center>
+    </div>
+       
+
+    </div>
+
+
+</body>
+</html>
+    
+</body>
+</html>
