2738397b1700637593a93460217f0d85ad04480c
anhhtu <vuanhtu0711@gmail.com>
2022-11-01 16:49:57 +0700

    fix menu, add footer

--- Login/view/login.php
+++ Login/view/login.php
@@ -15,6 +15,7 @@
 </head>
 
 <body>
+    <!--Navbar-->
     <div class="container-fluid d-flex justify-content-center" id="navbar">
         <nav class="navbar navbar-expand-md">
             <ul class="navbar-nav">
@@ -40,6 +41,7 @@
         </nav>
     </div>
 
+<div class="content">
     <div class="wrapper">
         <div class="form-login">
             <h1>Đăng nhập</h1>
@@ -60,6 +62,43 @@
             <img src="../src/images/logo.png" alt="">
         </div>
     </div>
+</div>
+
+    <!--Footer-->
+    <footer class="container-fluid py-5">
+        <div class="container">
+           <div class="row">
+                    <div class="col-md-3 ">
+                       <div class="logo-part">
+                          <img src="images/logo.png" class="w-75 logo-footer" style="margin: auto; display: block; padding-top: 18%;">
+                          <h4 style="margin-top: 7%; color: #DFDFDF;">Trung tâm Ứng cứu khẩn cấp không gian mạng Việt Nam</h4>
+                       </div>
+                    </div>
+                    <div class="col-md-7 px-4" style="line-height: 25px;">
+                       <h6 style="color: #DFDFDF;"> Thông tin liên hệ</h6>
+                       <span class="create"><img src="images/phone-call.png"/> Hotline:</span><br>
+                       <p class="contact-info">0869 100317</p> 
+                       <span class="create" style="padding-left: 20px;"> Fax:</span><br>
+                       <p class="contact-info">84.24.36404425</p> 
+                       <span class="create"><img src="images/mail.png"/> Email:</span><br>
+                       <p class="contact-info">office@vncert.vn</p>
+                       <span class="create"><img src="images/maps-and-flags.png"/> Địa chỉ:</span>
+                       <p class="contact-info">Tầng 5, 115 Trần Duy Hưng, Trung Hoà, Cầu Giấy, Hà Nội</p>
+                    </div>
+                    <div class="col-md-2">
+                        <h6 style="color: #DFDFDF;">Hỗ trợ</h6>
+                              <ul id="footer-menu">
+                                 <li> <a href="#"> Trang chủ</a> </li>
+                                 <li> <a href="#"> Tin tức</a> </li>
+                                 <li> <a href="#"> Đội ngũ</a> </li>
+                                 <li> <a href="#"> Tuyển dụng</a> </li>
+                                 <li> <a href="#"> Liên hệ</a> </li>
+                              </ul>
+                     </div>
+           </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
